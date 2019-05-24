f0c0678684bfeacfb9b2fe7a7c2b3dfb3eb76269
devchia254 <alex7chiaster@gmail.com>
2019-05-24 14:57:00 +0800

    Updates

---
+++ precache-manifest.7bb14ac43e1c1109d29e3b97a151c9f3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0d1de663ad244b8211c1bb2b4ccaf56",
+    "url": "/devchia254/starwars_api/index.html"
+  },
+  {
+    "revision": "eaeb2d132b6dcab0be1c",
+    "url": "/devchia254/starwars_api/static/css/2.2ec98f22.chunk.css"
+  },
+  {
+    "revision": "29997224faa0ff345fe0",
+    "url": "/devchia254/starwars_api/static/css/main.91b090c7.chunk.css"
+  },
+  {
+    "revision": "eaeb2d132b6dcab0be1c",
+    "url": "/devchia254/starwars_api/static/js/2.c846591c.chunk.js"
+  },
+  {
+    "revision": "29997224faa0ff345fe0",
+    "url": "/devchia254/starwars_api/static/js/main.e70bef38.chunk.js"
+  },
+  {
+    "revision": "b52dac58e689cc7e0f92",
+    "url": "/devchia254/starwars_api/static/js/runtime~main.08749e66.js"
+  },
+  {
+    "revision": "02980b307fdf1ec3fce5f53eaa190405",
+    "url": "/devchia254/starwars_api/static/media/Starjedi.02980b30.ttf"
+  },
+  {
+    "revision": "03f961de17b2e89cb5d80008546dd058",
+    "url": "/devchia254/starwars_api/static/media/Starjedi.03f961de.svg"
+  },
+  {
+    "revision": "6a5d63cff0c43a9294b5d91bf53d2845",
+    "url": "/devchia254/starwars_api/static/media/Starjedi.6a5d63cf.woff"
+  },
+  {
+    "revision": "7248a9a5e066780f6f8d42658b1be6c4",
+    "url": "/devchia254/starwars_api/static/media/Starjedi.7248a9a5.eot"
+  },
+  {
+    "revision": "ee7632779ceff1dbbec4bc7309aca5ee",
+    "url": "/devchia254/starwars_api/static/media/starwars_bg.ee763277.jpg"
+  }
+]);
